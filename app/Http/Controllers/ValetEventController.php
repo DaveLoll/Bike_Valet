17a67100bccb05c19f8ccd349e88570383a6f2c3
dloll <dloll@mail.mccneb.edu>
2017-10-14 11:58:04 -0500

    Created model and controller shells.

---
+++ app/Http/Controllers/ValetEventController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ValetEvent;
+use Illuminate\Http\Request;
+
+class ValetEventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ValetEvent  $valetEvent
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ValetEvent $valetEvent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ValetEvent  $valetEvent
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ValetEvent $valetEvent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ValetEvent  $valetEvent
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ValetEvent $valetEvent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ValetEvent  $valetEvent
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ValetEvent $valetEvent)
+    {
+        //
+    }
+}
