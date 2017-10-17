d82d4cfdc8c79f5ae04ebe299b8777eef0aa48b5
waholland <waholland@mail.mccneb.edu>
2017-10-17 18:48:02 -0500

    Initial work of creating blade pages

---
+++ resources/views/ListBikes.blade.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <title>@yield('title')</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Omaha Bike Valet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.css">
+    <link rel="stylesheet" href = {{asset('/css/style.css')}}>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js"></script>
+</head>
+<body>
+<div data-role="page">
+    <div data-role="header">
+
+    </div><!-- /header -->
+
+    <div role="main" class="ui-content">
+        
+    </div>
+
+    <div data-role="footer">
+
+    </div>
+</div>
+</body>
+</html>
