bd2a4b9d83e2772025b67b328092ddabd13a1c2b
Allen L Clark <aclark49@mail.mccneb.edu>
2017-11-07 14:27:47 -0600

    Quick tweak to fix "Login" button rendering

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -68,8 +68,8 @@ Log:
             </div>
             <p><a href="{{url('/register')}}">NEED TO REGISTER?</a></p>
 
-            <div class="ui-input-btn ui-corner-all">
-                <input type="submit" name="submit" id="submit" value="Login">
+            <div class="ui-input-btn ui-corner-all" input type="submit" name="submit" id="submit" value="Login">
+            LOGIN
             </div>
         </form>
 
